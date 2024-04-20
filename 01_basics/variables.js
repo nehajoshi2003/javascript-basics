fc7f0fdd4af9e36356b0ee6a77a41f1f22e27ec2
nehajoshi2003 <113055355+nehajoshi2003@users.noreply.github.com>
2024-04-20 13:51:46 +0000

    basics-started

---
+++ 01_basics/variables.js
@@ -0,0 +1,11 @@
+const accountId = 144553
+let accountEmail="neha@gmail.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+
+// accountId = 2 // not allowed
+accountEmail="chinu@gmail.com"
+accountPassword="56789"
+accountCity="Banglore"
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity])
